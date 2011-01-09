6c3d7335885966703c13598dafc47949d33452bc
littish <littish@gmail.com>
2011-01-09 15:22:28 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Offertaa::Application.config.secret_token = '0a432b5dd6bd7032f333999b3d109ba167a213b60ba463b071c1bd3c60fa1a4ed5f1812a09d1002fe3dbddf106057d85aae4bef06860bad739232528352fb73a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Offertaa::Application.config.session_store :cookie_store, :key => '_offertaa_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Offertaa::Application.config.session_store :active_record_store
